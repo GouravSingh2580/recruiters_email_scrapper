8b72f6c8db1455736aacb5b16b5887c28238c3eb
Gourav Singh <49956754+GouravSingh2580@users.noreply.github.com>
2022-02-24 18:23:11 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+const puppeteer = require('puppeteer');
+const fs = require('fs');
+const ObjectsToCsv = require('objects-to-csv');
+const csvReader = require('csv-parser');
+var query = require('cli-interact').getYesNo;
+const log = console.log;
+
+const scrapeCompany = async (companyName, searchTerm) => {
+    // LOCAL RUNNING ON COMPUTER
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+    const page = await browser.newPage();
+    await page.goto('https://google.com');
+    let company = companyName.toLowerCase();
+    let myLocalValue = `site:linkedin.com \"@${company}.com\" \"${searchTerm}\" email`;
+    await page.$eval('input[name=q]', (el, value) => el.value = value, myLocalValue);
+    page.keyboard.press('Enter');
+    await page.waitForNavigation();
+    return await page.evaluate((company) => {
+        function extractEmails(text) {
+            return text.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9_-]+)/gi);
+        }
+        let results = [...document.querySelectorAll('.g')];
+        let temp = [];
+        for (const elem of results) {
+            temp.push({
+                company: company,
+                link: elem.querySelector('a').href.replace(/(\r\n|\n|\r)/gm, ""),
+                description: elem.innerText.replace(/(\r\n|\n|\r)/gm, ""),
+                email: extractEmails(elem.innerText)
+            });
+        }
+        return temp;
+    }, company);
+    browser.close();
+};
+
+const driver = async () => {
+    // Variable to change if you want to specify search term
+    var searchTerm = 'recruiter';
+    // Do not touch
+    var arr = [];
+    var results = [];
+    log('Hello, please make sure that the input.csv file has been written with the list of companies you want');
+    log('type y to continue');
+    ans = query('');
+    if (!ans) {
+        log('program shutting down, please restart');
+        return;
+    }
+    fs.createReadStream('input.csv')
+        .pipe(csvReader())
+        .on('data', (row) => {
+            arr.push(row.company);
+        })
+        .on('end', async () => {
+            console.log('CSV file successfully processed');
+            for (const company of arr) {
+                console.log(`Company ${company} is being processed`);
+                companySearch = await scrapeCompany(company, searchTerm);
+                results = [...results, ...companySearch];
+            }
+            let data = results;
+            const csv = new ObjectsToCsv(data);
+            await csv.toDisk('./output.csv');
+            log('CSV file successfully written, please check the output.csv file');
+        });
+};
+
+driver();
